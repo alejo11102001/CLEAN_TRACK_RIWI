8baad52d048633fb91088b588e9447a4ef68b60d
Jrestrepo18 <jerorrpo@gmail.com>
2025-08-25 21:19:50 -0500

    new changes

--- front-end/js/zones_employee.js
+++ front-end/js/zones_employee.js
@@ -171,3 +171,27 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 });
+
+if ('serviceWorker' in navigator) {
+window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./sw.js')
+    .then(registration => {
+        console.log('Service Worker registrado con éxito:', registration);
+    })
+    .catch(error => {
+        console.log('Fallo en el registro del Service Worker:', error);
+    });
+});
+}
+
+if ('serviceWorker' in navigator) {
+window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./sw.js')
+    .then(registration => {
+        console.log('Service Worker registrado con éxito:', registration);
+    })
+    .catch(error => {
+        console.log('Fallo en el registro del Service Worker:', error);
+    });
+});
+}
